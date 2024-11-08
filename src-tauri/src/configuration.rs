beb7cfbe42e9f35605c2b09f36c07f63888314fc
sarwin_ <64829298+SarwinR@users.noreply.github.com>
2024-11-08 17:56:31 +0400

    Configuration file
    Attempts to load the config file. Create one with default values if it does not exist.

---
+++ src-tauri/src/configuration.rs
@@ -0,0 +1,53 @@
+use std::{fs, path::PathBuf};
+use tauri::{App, Manager};
+use tauri_plugin_dialog::{DialogExt, MessageDialogKind};
+use yaml_rust2::{Yaml, YamlLoader};
+
+pub fn load_and_setup_configuration(app: &App) -> Result<Yaml, Box<dyn std::error::Error>> {
+    let config_file_path = app.path().home_dir()?.join(".quicksheet\\config.yaml");
+
+    check_and_create_config_file(&config_file_path)?;
+    let general_config = load_configuration(&app, &config_file_path)?;
+
+    Ok(general_config)
+}
+
+fn check_and_create_config_file(
+    config_file_path: &PathBuf,
+) -> Result<(), Box<dyn std::error::Error>> {
+    if !config_file_path.try_exists()? {
+        //todo: copy the default configuration file to the config file path
+        fs::create_dir_all(config_file_path.parent().unwrap())?;
+        fs::write(
+            &config_file_path,
+            "general:\n- shortcut: [\"ctrl+shift+enter\"]",
+        )?;
+    }
+
+    Ok(())
+}
+
+fn load_configuration(
+    app: &App,
+    config_file_path: &PathBuf,
+) -> Result<Yaml, Box<dyn std::error::Error>> {
+    let configuration_string = fs::read_to_string(config_file_path)?;
+
+    let configs = match YamlLoader::load_from_str(&configuration_string) {
+        Ok(configs) => configs,
+        Err(_) => {
+            app
+                .dialog()
+                .message("The configuration file is invalid. Please check the configuration file and try again.")
+                .kind(MessageDialogKind::Error)
+                .title("Quicksheet Invalid Configuration")
+                .blocking_show();
+            return Err("Invalid configuration file".into());
+        }
+    };
+
+    // todo: check if the configuration file is valid
+
+    let general_config = configs[0].clone();
+    Ok(general_config)
+}
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,18 +1,25 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+mod configuration;
 mod global_shortcut;
 mod system_tray_menu;
 mod webview;
 
 fn main() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_dialog::init())
         .setup(|app| {
             let handle = app.handle();
 
-            system_tray_menu::initialize_system_tray_menu(app)?; // Initialize the menu
-            webview::initialize_webview(&handle)?; // Initialize the webview window
-            global_shortcut::initialize_shortcut(app, handle.clone())?; // Initialize global shortcuts
+            let general_config = configuration::load_and_setup_configuration(app)?;
+
+            system_tray_menu::initialize_system_tray_menu(app)?;
+
+            webview::initialize_webview(&handle)?;
+
+            // todo: use the general_config to setup the application [only shortcut keys for now]
+            global_shortcut::initialize_shortcut(app, handle.clone())?;
 
             Ok(())
         })
