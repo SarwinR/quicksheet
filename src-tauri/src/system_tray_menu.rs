06b545c6c38d8ab96246898b66cb3556882b3ce4
sarwin_ <64829298+SarwinR@users.noreply.github.com>
2025-05-02 21:57:59 +0400

    "Open Config" Option works #4

--- src-tauri/src/system_tray_menu.rs
+++ src-tauri/src/system_tray_menu.rs
@@ -1,4 +1,5 @@
-use tauri::{menu::Menu, menu::MenuItem, tray::TrayIconBuilder, App};
+use std::process::Command;
+use tauri::{menu::Menu, menu::MenuItem, tray::TrayIconBuilder, App, Manager};
 
 pub fn initialize_system_tray_menu(app: &App) -> tauri::Result<()> {
     let open_config_i = MenuItem::with_id(app, "open-config", "Open Config", true, None::<&str>)?;
@@ -9,9 +10,21 @@ pub fn initialize_system_tray_menu(app: &App) -> tauri::Result<()> {
         .icon(app.default_window_icon().unwrap().clone())
         .menu(&menu)
         .menu_on_left_click(true)
-        .on_menu_event(|app, event| match event.id.as_ref() {
-            "open-config" => println!("open-config menu item was clicked"),
-            "quit" => app.exit(0),
+        .on_menu_event(|app_handle, event| match event.id.as_ref() {
+            "open-config" => {
+                let app_handle = app_handle.clone();
+                let config_file_path = app_handle.path().home_dir().unwrap().join(".quicksheet");
+                
+                #[cfg(target_os = "windows")]
+                let _ = Command::new("explorer").arg(config_file_path).spawn();
+
+                #[cfg(target_os = "macos")]
+                let _ = Command::new("open").arg(config_file_path).spawn();
+
+                #[cfg(target_os = "linux")]
+                let _ = Command::new("xdg-open").arg(config_file_path).spawn();
+            }
+            "quit" => app_handle.exit(0),
             _ => println!("menu item {:?} not handled", event.id),
         })
         .build(app)?;
